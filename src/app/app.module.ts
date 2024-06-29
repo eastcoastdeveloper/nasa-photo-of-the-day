c62a0daa52cbf259af2f63179bffad9692ba4cf2
eastcoastdeveloper <uxdeveloper@protonmail.com>
2024-06-29 13:44:49 -0700

    updating

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,28 +2,21 @@ import { NgModule } from "@angular/core";
 import { BrowserModule } from "@angular/platform-browser";
 import { AppComponent } from "./app.component";
 import { CalendarComponent } from "./components/date-picker/date-picker.component";
-import { HttpClientModule } from "@angular/common/http";
+import { provideHttpClient, withInterceptorsFromDi } from "@angular/common/http";
 import { WindowRef } from "./windowRef";
 import { NasaHeaderComponent } from "./components/header/header.component";
 import { NasaSearchComponent } from "./components/seachbar/search.component";
 import { AppRoutingModule, appRoutingComponents } from "./app-routing.module";
 import { YouTubePlayerModule } from "@angular/youtube-player";
 
-@NgModule({
-  declarations: [
-    AppComponent,
-    CalendarComponent,
-    appRoutingComponents,
-    NasaHeaderComponent,
-    NasaSearchComponent,
-  ],
-  imports: [
-    BrowserModule,
-    HttpClientModule,
-    YouTubePlayerModule,
-    AppRoutingModule,
-  ],
-  providers: [WindowRef],
-  bootstrap: [AppComponent],
-})
+@NgModule({ declarations: [
+        AppComponent,
+        CalendarComponent,
+        appRoutingComponents,
+        NasaHeaderComponent,
+        NasaSearchComponent,
+    ],
+    bootstrap: [AppComponent], imports: [BrowserModule,
+        YouTubePlayerModule,
+        AppRoutingModule], providers: [WindowRef, provideHttpClient(withInterceptorsFromDi())] })
 export class AppModule {}
