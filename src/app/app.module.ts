47cbf629fc52a2ab497fd3e1d5572f1d8744b230
eastcoastdeveloper <uxdeveloper@protonmail.com>
2024-06-30 11:56:09 -0700

    updating

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,21 +2,26 @@ import { NgModule } from "@angular/core";
 import { BrowserModule } from "@angular/platform-browser";
 import { AppComponent } from "./app.component";
 import { CalendarComponent } from "./components/date-picker/date-picker.component";
-import { provideHttpClient, withInterceptorsFromDi } from "@angular/common/http";
+import { HttpClient, provideHttpClient, withInterceptorsFromDi } from "@angular/common/http";
 import { WindowRef } from "./windowRef";
 import { NasaHeaderComponent } from "./components/header/header.component";
 import { NasaSearchComponent } from "./components/seachbar/search.component";
-import { AppRoutingModule, appRoutingComponents } from "./app-routing.module";
+// import { AppRoutingModule, appRoutingComponents } from "./app-routing.module";
 import { YouTubePlayerModule } from "@angular/youtube-player";
 
-@NgModule({ declarations: [
-        AppComponent,
-        CalendarComponent,
-        appRoutingComponents,
-        NasaHeaderComponent,
-        NasaSearchComponent,
-    ],
-    bootstrap: [AppComponent], imports: [BrowserModule,
-        YouTubePlayerModule,
-        AppRoutingModule], providers: [WindowRef, provideHttpClient(withInterceptorsFromDi())] })
+@NgModule({
+  declarations: [
+    AppComponent,
+    CalendarComponent,
+    // appRoutingComponents,
+    NasaHeaderComponent,
+    NasaSearchComponent,
+  ],
+  bootstrap: [AppComponent],
+  imports: [
+    BrowserModule,
+    YouTubePlayerModule,
+    // AppRoutingModule], providers: [WindowRef, provideHttpClient(withInterceptorsFromDi())
+  ],
+})
 export class AppModule {}
